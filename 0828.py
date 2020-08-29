c372088a3a608c9d3c355fcfa288a0a921ee45b4
Lica3265 <36996636+Lica3265@users.noreply.github.com>
2020-08-29 14:15:39 +0800

    我寫的答案

--- 0828.py
+++ 0828.py
@@ -1,3 +1,12 @@
 from collections import Counter
-Value1=input("請輸入英文字符:")
-print(Counter(Value1))
+
+Value1= input("請輸入英文字母:")
+result= ''
+dictForSorting = dict(Counter(Value1))
+print(dictForSorting)
+sortedData =sorted(dictForSorting.items(),key=lambda  x:x[1],reverse=True)
+print(sortedData)
+for item in sortedData:
+    print(item)
+    result +=  item[0]*item[1]
+print(result)
